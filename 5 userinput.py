a1c5b20a5566ecdd052fb8823e7dc7f54744b86f
Taha Ahmed <taha40636@gmail.com>
2025-06-02 15:39:36 +0500

    loop topic is covered

--- 5 userinput.py
+++ 5 userinput.py
@@ -1,6 +1,12 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
 
-print("The sum is", a + b)
-print("the multiplication number is", a*b)
-print("the division is", a/b)
+# print("The sum is", a + b)
+# print("the multiplication number is", a*b)
+# print("the division is", a/b) 
+
+a=(input("Enter your name: "))
+print("My name is ", a)
+
+b=float(input("Enter your balance: "))
+print("your balance is ", b)   
---
+++ 6 conditionalstatement.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+if a>b and a>0:
+    print(a,"is greater than", b ,"and it is positive number")
+elif a==b:
+    print(a, "is equal to", b)
+else:
+    print(a,"is smaller than",b)
+
+
+    
