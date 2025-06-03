7f4a97ced40663447c437ccd196b2ef20e49369d
Taha Ahmed <taha40636@gmail.com>
2025-06-03 15:26:39 +0500

    variables defined

--- 6 conditionalstatement.py
+++ 6 conditionalstatement.py
@@ -1,5 +1,5 @@
 a=int(input("Enter the value of a: "))
-b=int(input("Enter the value of b: ")) # this is a comment for learning purpose 
+b=int(input("Enter the value of b: ")) # this is a comment for learning purpose here it's
 if a>b and a>0:
     print(a,"is greater than", b ,"and it is positive number")
 elif a==b:
